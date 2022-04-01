910b6f2cf05bf1bcc7fa230689a3f3b5537c2938
Piet Cordemans <cordemans.piet@gmail.com>
2022-04-01 09:23:54 +0200

    timer with interrupts

--- main.cpp
+++ main.cpp
@@ -2,50 +2,57 @@
 #include "stm32l476xx.h"
 #include "mbed.h"
 
-void led_init(){
+void led_toggle()
+{
+    // Read output PA_5
+    if (GPIOA->ODR & (1 << 5))
+    {
+        // bit reset
+        GPIOA->BSRR |= 1 << 21;
+    }
+    else
+    {
+        // bit set
+        GPIOA->BSRR |= 1 << 5;
+    }
+}
+
+extern "C"{
+void TIM7_IRQHandler(){
+    led_toggle();
+    TIM7->SR = 0;
+}
+}
+
+void led_init()
+{
     //Enable GPIOA
     RCC->AHB2ENR |= 1;
     //Select PA_5 as output
     GPIOA->MODER |= 1 << 10;
     GPIOA->MODER &= ~(1 <<11);
-
 }
 
 void timer_init(){
     //Enable Timer 7
     RCC->APB1ENR1 |= 1 << 5;
+    NVIC_EnableIRQ(TIM7_IRQn);
     //Reinitializes Timer 7 after event
     TIM7->EGR |= 1;
     //Set Timer 7 match with fosc = 48MHz
     TIM7->ARR = 47999;
     TIM7->PSC = 999;
+    TIM7->DIER |= 1;
 }
 
-void led_toggle(){
-    // Read output PA_5
-    if(GPIOA->ODR & (1 << 5))
-    {
-        // bit reset
-        GPIOA->BSRR |= 1 << 21;
-    }
-    else 
-    {
-        // bit set
-        GPIOA->BSRR |= 1 << 5;
-    }
-    
-    
-}
+
 
 void timer_start(){
     //enable timer
     TIM7->CR1 |= 1;
 }
 
-void timer_wait(){
-    while(!(TIM7->SR & 1)){}
-    TIM7->SR = 0;
-}
+
 
 int main()
 {
@@ -53,10 +60,8 @@ int main()
     timer_init();
     timer_start();
 
-    while(true){
+    while(true){       
         
-        led_toggle();
-        //wait here
-        timer_wait();
+        
     }
 }
